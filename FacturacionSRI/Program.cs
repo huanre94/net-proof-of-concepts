8ba80180af499019085052990e23a0ef3bab66ff
Hugo Restrepo <hrestrepo@sipecom.com>
2024-11-06 14:08:54 -0500

    Importacion clase desde xsd

---
+++ FacturacionSRI/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Hello, World!");
+
+var factura = new factura();
+factura.id = facturaID.comprobante;
+factura.version = "1";
+
+var datosTributarios = new infoTributaria();
+datosTributarios.claveAcceso = "";
+datosTributarios.contribuyenteRimpe = "";
+
+var infoFactura = new facturaInfoFactura { };
+
+factura.infoFactura = infoFactura;
+
+
+var detalles = new facturaDetalle
+{
+    descripcion = "prueba",
+    precioUnitario = 1,
+    codigoPrincipal = "001"
+};
+
+
+
+Console.WriteLine(factura);
---
+++ FacturacionSRI/README.md
@@ -0,0 +1,4 @@
+# Cargar archivo xsd  
+## Linea para cargar nuevo elemento en base a .xsd
+### Debo localizar primero el xsd en directorio  
+> PS C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools> .\xsd.exe /c "C:\Users\HRESTRE\source\repos\ConsoleApp1\FacturacionSRI\xsd\factura_V1.0.0.xsd" "C:\Users\HRESTRE\source\repos\ConsoleApp1\FacturacionSRI\xsd\xmldsig-core-schema.xsd" /out:"C:\Users\HRESTRE\source\repos\ConsoleApp1\FacturacionSRI\"   
