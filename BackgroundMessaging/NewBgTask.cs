8c029d7d8ff6c776c68a521f9028b54453905b5e
Hugo Restrepo <hrestrepo@sipecom.com>
2024-11-11 16:41:06 -0500

    Background Operation

---
+++ BackgroundMessaging/NewBgTask.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BackgroundMessaging
+{
+    public class NewBgTask : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine($"{DateTime.Now}: Ejecutando proceso en segundo plano");
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
